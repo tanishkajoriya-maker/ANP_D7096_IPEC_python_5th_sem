60305c7cc421453c0f8b00d0104e881d1ce56524
Tanishka Joriya <tanishkajoriya@gmail.com>
2026-07-07 09:26:56 +0530

    uppercase and lowercase

---
+++ classwork/day9_string_6_july/to_count_uppercase_and_lowercase.py
@@ -0,0 +1,13 @@
+# Program to count uppercase and lowercase letters in a string
+'''Problem Statement: Write a program to count the uppercase characters as well as lowercase characters is given sentence, without using library function.'''
+#-------------------------------------------------coding----------------------------------------------------------
+sentence = input("Enter a sentence: ")
+uppercase_count = 0
+lowercase_count = 0
+for char in sentence:
+    if char.isupper():
+        uppercase_count += 1
+    elif char.islower():
+        lowercase_count += 1
+print("Uppercase letters:", uppercase_count)
+print("Lowercase letters:", lowercase_count)
